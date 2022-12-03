a0e3917e6bf13cdb6696ce52b437167174e176c1
TheoLomeuBraga <theolomeu@gmail.com>
2022-12-03 19:49:20 -0300

    a

---
+++ install-basics.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip install pysimplegui
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update
+sudo apt install -y debootstrap arch-install-scripts
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo sh install-basics.sh
