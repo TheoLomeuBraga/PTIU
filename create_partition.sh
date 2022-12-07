367c4d8c56c30f51c2ca96d73a629eaf5918c47c
TheoLomeuBraga <theolomeu@gmail.com>
2022-12-07 13:15:57 -0300

    a

--- create_partition.sh
+++ create_partition.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 device="/dev/"$1
-home_size_mb=$2
-swap_size_mb=$3+1
+home_size_mb=$(($2))
+swap_size_mb=$(($3+1))
 
-bios_size_mb=500
+bios_size_mb=$((500))
 
-begin_bios_mb=1
-end_bios_mb=$begin_bios_mb+$bios_size_mb+1
+begin_bios_mb=$((1))
+end_bios_mb=$(($begin_bios_mb+$bios_size_mb+1))
 
-begin_swap_mb=$end_bios_mb
-end_swap_mb=$begin_swap_mb+$swap_size_mb+1
+begin_swap_mb=$(($end_bios_mb))
+end_swap_mb=$(($begin_swap_mb+$swap_size_mb+1))
 
-begin_home_mb=$end_swap_mb+1
-end_home_mb=$begin_home_mb+$home_size_mb+1
+begin_home_mb=$(($end_swap_mb+1))
+end_home_mb=$(($begin_home_mb+$home_size_mb+1))
 
 if home_size_mb gt 0
 then
@@ -32,12 +32,19 @@ EOF
 else
 echo "no home partition"
 sudo parted $device -s mklabel gpt
-sudo parted $device -s mkpart EFI fat32 ${begin_bios_mb}MB ${end_bios_mb}MB
-sudo parted $device -s mkpart swap linux-swap ${begin_swap_mb}MB ${end_swap_mb}MB
-sudo parted $device -s mkpart main ext4 ${end_swap_mb}MB 100%
 
+sudo parted $device -s mkpart EFI fat32 ${begin_bios_mb}MB ${end_bios_mb}MB
 sudo mkfs.vfat -F 32 ${device}1
 
+sudo parted $device -s mkpart swap linux-swap ${begin_swap_mb}MB ${end_swap_mb}MB
 sudo mkswap ${device}2
+
+sudo parted $device -s mkpart main ext4 ${end_swap_mb}MB 100%
+sudo mkfs -t ext4 ${device}3
+
+
+
+
+
 
 fi
