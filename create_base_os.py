fb079d9725b9e11ece59ae4c294bf08514793fde
TheoLomeuBraga <theolomeu@gmail.com>
2022-12-08 14:22:11 -0300

    a

--- create_base_os.py
+++ create_base_os.py
@@ -5,6 +5,7 @@ def create_base_os():
     debootstrap_command = "sudo debootstrap focal /mnt https://mirror.leaseweb.com/ubuntu/"
     ret.append(debootstrap_command)
     ret.append("sudo cd /usr/share/debootstrap/scripts && ln -sf gutsy jammy")
+    ret.append("linux cat /etc/resolv.conf > mnt/etc/resolv.conf")
     return ret
 
 
