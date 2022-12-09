7aae9071ca97565988adea42a467dd6849db30ce
TheoLomeuBraga <theolomeu@gmail.com>
2022-12-08 21:58:50 -0300

    a

--- create_partition.sh
+++ create_partition.sh
@@ -4,12 +4,14 @@ device="/dev/"$1
 home_size_mb=$(($2))
 swap_size_mb=$(($3+1))
 
-bios_size_mb=$((500))
+efi_size_mb=$((500))
 
-begin_bios_mb=$((1))
-end_bios_mb=$(($begin_bios_mb+$bios_size_mb+1))
+boot_size_mb=$((500))
 
-begin_swap_mb=$(($end_bios_mb))
+begin_efi_mb=$((1))
+end_efi_mb=$(($begin_efi_mb+$efi_size_mb+1))
+
+begin_swap_mb=$(($end_efi_mb))
 end_swap_mb=$(($begin_swap_mb+$swap_size_mb+1))
 
 begin_home_mb=$(($end_swap_mb+1))
@@ -21,7 +23,7 @@ echo "with home partition"
 
 sudo parted $device -s mklabel gpt
 
-sudo parted $device -s mkpart EFI fat32 ${begin_bios_mb}MB ${end_bios_mb}MB
+sudo parted $device -s mkpart EFI fat32 ${begin_efi_mb}MB ${end_efi_mb}MB
 sudo mkfs.vfat -F 32 ${device}1
 
 sudo parted $device -s mkpart swap linux-swap ${begin_swap_mb}MB ${end_swap_mb}MB
@@ -46,7 +48,7 @@ else
 echo "no home partition"
 sudo parted $device -s mklabel gpt
 
-sudo parted $device -s mkpart EFI fat32 ${begin_bios_mb}MB ${end_bios_mb}MB
+sudo parted $device -s mkpart EFI fat32 ${begin_efi_mb}MB ${end_efi_mb}MB
 sudo mkfs.vfat -F 32 ${device}1
 
 sudo parted $device -s mkpart swap linux-swap ${begin_swap_mb}MB ${end_swap_mb}MB
