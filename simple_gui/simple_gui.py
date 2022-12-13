d6b86a626a4770e874fdcee632889d6891cd4ff2
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2022-12-13 16:23:32 -0300

    a

--- simple_gui/simple_gui.py
+++ simple_gui/simple_gui.py
@@ -18,7 +18,16 @@ window_list = []
 
 window = Tk()
 
-is_a_test = True
+is_a_test = False
+
+args = []
+if __name__ == "__main__":
+    for i, arg in enumerate(sys.argv):
+        args.append(arg)
+
+for a in args:
+    if a == "test":
+        is_a_test = True
 
 def next():
     global window_id,window
@@ -34,7 +43,7 @@ def previous():
     window.destroy()
     window = Tk()
     window_list[window_id].start(window)
-
+    install_configs.pop()
 
 
 
@@ -70,7 +79,7 @@ class welcome_window:
 window_list.append(welcome_window())
 
 
-class select_partition_window():
+class select_partition_window:
     def __init__(self):
         print("")
         self.avaliable_device_list = ["sda","sdb","sdc","sde","sdf"]
@@ -103,13 +112,13 @@ class select_partition_window():
 
         
         list_items = Variable(value=self.avaliable_device_list)
-        self.device_list = Listbox(partition_frame,height = 6,width = 10,listvariable=list_items)
+        self.device_list = Listbox(partition_frame,height = 10,width = 20,listvariable=list_items)
         self.device_list.pack(side=TOP)
 
 
         #final
         final_frame = Frame(window,borderwidth=0,relief="raised")
-        final_frame.place(x=50,y=175,width=200,height=150)
+        final_frame.place(x=50,y=250,width=200,height=50)
         final_frame["background"]=background_color
 
         next_button = Button(final_frame,text="next>>",command=next)
@@ -124,10 +133,66 @@ class select_partition_window():
     def ok(self):
         conf = ins.instalaton_config()
         conf.deviceice = self.avaliable_device_list[self.device_list.curselection()[0]]
-        print(self.avaliable_device_list[self.device_list.curselection()[0]])
-        install_configs.append(conf)
-        
+        install_configs.append(conf)     
 window_list.append(select_partition_window())
+
+class select_wm_window:
+    def __init__(self):
+        print("")
+        self.avaliable_wm_list = ["xfce","mate","lxqt","kde"]
+        self.avaliable_wm_list += ["","","","","","",""]
+    def start(self,window):#https://www.pythontutorial.net/tkinter/tkinter-listbox/
+        
+        window.title("warning")
+        window.geometry("300x300")
+        window.iconbitmap("@"+os.getcwd()+"/PTIU.xbm")
+        window["background"]=background_color
+
+        #warning
+        fm = Frame(window,borderwidth=1,relief="raised")
+        fm.place(x=10,y=10,width=50,height=50)
+        fm["background"]=background_color
+
+        text = Label(fm, text = "to proceed with the installation you must:\n select a device without partitions",background="light blue")
+        text.pack(side=TOP)
+
+        #partition frame
+        partition_frame = Frame(window,borderwidth=0,relief="raised")
+        partition_frame.place(x=50,y=75,width=200,height=200)
+        partition_frame["background"]=background_color
+    
+        
+
+        
+        list_items = Variable(value=self.avaliable_wm_list)
+        self.wm_list = Listbox(partition_frame,height = 10,width = 20,listvariable=list_items)
+        self.wm_list.pack(side=TOP)
+
+
+        #final
+        final_frame = Frame(window,borderwidth=0,relief="raised")
+        final_frame.place(x=50,y=250,width=200,height=50)
+        final_frame["background"]=background_color
+
+        next_button = Button(final_frame,text="next>>",command=next)
+        next_button["background"]=background_color
+        next_button.pack(side=RIGHT)
+
+        previous_button = Button(final_frame,text="<<previous",command=previous)
+        previous_button["background"]=background_color
+        previous_button.pack(side=LEFT)
+
+        fm.pack(fill=BOTH, expand=YES)
+    def ok(self):
+        conf = ins.instalaton_config()
+        # "xfce" "mate" "lxqt" "kde"
+        selected_wm = self.wm_list.curselection()[0]
+        
+        
+        install_configs.append(conf)    
+window_list.append(select_wm_window())
+
+
 
 
 class terminal_warning_window:
