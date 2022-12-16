49ecee6f6beb6bea78fd6e4e6181daf6ff19ee3e
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2022-12-16 19:55:27 -0300

    a

--- ptiu_lib/cmd_manager.py
+++ ptiu_lib/cmd_manager.py
@@ -2,14 +2,21 @@ import os
 
 cmds = []
 
-
-
 def add_cmd(cmd):
     cmds.append(cmd)
 
 def add_cmds(commands):
     global cmds
     cmds = cmds + commands 
+
+cmds_cr = []
+
+def add_cmd_cr(cmd):
+    cmds_cr.append(cmd)
+
+def add_cmds_cr(commands):
+    global cmds_cr
+    cmds_cr = cmds_cr + commands 
 
 def begin_installation():
     print("begin installation")
@@ -19,6 +26,15 @@ def begin_installation():
      
     f = open("command.sh", "w")
     for c in cmds:
+        f.write(c+"\n")
+    f.close()
+
+    global cmds
+    if os.path.isfile("command_cr.sh"):
+        os.remove("command_cr.sh")
+     
+    f = open("command_cr.sh", "w")
+    for c in cmds_cr:
         f.write(c+"\n")
     f.close()
 
