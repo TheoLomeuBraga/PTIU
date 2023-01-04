fb1248c97d307cbc1b1b7297b36139b9d210155b
TheoLomeuBraga <theolomeu@gmail.com>
2023-01-03 21:15:37 -0300

    a

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,20 +1,26 @@
 sudo sh install_dependences.sh
 
+#copy to /usr/share
 sudo rm -r /usr/share/technomancy_instaler
-
 sudo cp -t ./ /usr/share
 
+#change name to technomancy_instaler
+sudo cd /usr/share 
+sudo mv PTIU technomancy_instaler
+sudo cd -
 
-echo "[Desktop Entry]" >> /usr/share/applications/openbox_config_center.desktop
-echo "Encoding=UTF-8" >> /usr/share/applications/openbox_config_center.desktop
-echo "Version=1.0" >> /usr/share/applications/openbox_config_center.desktop
-echo "Type=Application" >> /usr/share/applications/openbox_config_center.desktop
-echo "Terminal=false" >> /usr/share/applications/openbox_config_center.desktop
-echo "Exec=python3 /usr/share/openbox_config_center/center.py" >> /usr/share/applications/openbox_config_center.desktop
-echo "Path=/usr/share/openbox_config_center/" >> /usr/share/applications/openbox_config_center.desktop
-echo "Type=Application" >> /usr/share/applications/openbox_config_center.desktop
-echo "Name=openbox configurations center" >> /usr/share/applications/openbox_config_center.desktop
-echo "Icon=/usr/share/openbox_config_center/config_icon.png" >> /usr/share/applications/openbox_config_center.desktop
+#
+sudo touch /usr/share/applications/technomancy_instaler.desktop
+echo "[Desktop Entry]" >> /usr/share/applications/technomancy_instaler.desktop
+echo "Encoding=UTF-8" >> /usr/share/applications/technomancy_instaler.desktop
+echo "Version=1.0" >> /usr/share/applications/technomancy_instaler.desktop
+echo "Type=Application" >> /usr/share/applications/technomancy_instaler.desktop
+echo "Terminal=true" >> /usr/share/applications/technomancy_instaler.desktop
+echo "Exec=sh /usr/share/technomancy_instaler/un.sh" >> /usr/share/applications/technomancy_instaler.desktop
+echo "Path=/usr/share/technomancy_instaler/" >> /usr/share/applications/technomancy_instaler.desktop
+echo "Type=Application" >> /usr/share/applications/technomancy_instaler.desktop
+echo "Name=technomancy instaler center" >> /usr/share/applications/technomancy_instaler.desktop
+echo "Icon=/usr/share/technomancy_instaler/install_icon.png" >> /usr/share/applications/technomancy_instaler.desktop
 
 
 
